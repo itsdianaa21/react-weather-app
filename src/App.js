eaafdd22ad4381885a46a27331fed65296900a4f
itsdianaa21 <dianamenendez17@gmail.com>
2023-07-25 12:24:22 -0500

    deleted files and unnecessary data from certain files, added css
    Creating base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather />
+        <footer>
+          {" "}
+          This project was created by Diana Menendez and is
+          <a href="https://github.com/itsdianaa21" target="_blank">
+            {" "}
+            open-sourced{" "}
+          </a>
+          on Github.
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
