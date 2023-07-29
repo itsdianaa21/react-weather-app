bde21492493d8767ff303121385a8c88b6c95524
itsdianaa21 <dianamenendez17@gmail.com>
2023-07-29 17:55:51 -0500

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -27,5 +27,13 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-    )
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
